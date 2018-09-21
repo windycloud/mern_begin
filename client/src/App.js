0481c95b4b4296038ec9f7d3d89fe8620198adb7
le thanh long <thanhlong.vn214@gmail.com>
2018-09-21 17:28:52 +0700

    learn 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList';
+import {Provider} from 'react-redux';
+import store from './store';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,10 +11,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
