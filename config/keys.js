56fa6396ffaa6abecea5d0646ef05ae7136be320
le thanh long <thanhlong.vn214@gmail.com>
2018-09-26 14:17:39 +0700

    change connect mongoDb to mLab

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://localhost:27017/mern_shopping'
+	// mongoURI: 'mongodb://localhost:27017/mern_shopping'
+	mongoURI: 'mongodb://phong:123456789lL.@ds115283.mlab.com:15283/mern_shopping',
 }
