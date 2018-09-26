fe6e35f0b2983f39989cdd3d474e7b947e97da2b
le thanh long <thanhlong.vn214@gmail.com>
2018-09-26 13:52:24 +0700

    add new file

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,21 +1,57 @@
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from './types';
+import	axios from 'axios';
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING } from './types';
 
-export const getItems = () => {
-	return {
-		type: GET_ITEMS,
-	}
+export const getItems = () => dispatch => {
+	// return {
+	// 	type: GET_ITEMS,
+	// }
+	dispatch(setItemsLoading());
+	axios
+		.get('/api/items')
+		.then(
+			res => dispatch({
+				type: GET_ITEMS,
+				payload: res.data
+			})
+		)
 }
 
-export const deleteItem = id => {
+export const addItem = (item) => dispatch => {
+	// return {
+	// 	type: ADD_ITEMS,
+	// 	payload: item
+	// }
+
+	axios
+		.post('/api/items', item)
+		// .then(function (response) {
+		//   console.log(response);
+		// })
+		.then(res => dispatch ({
+			 	type : ADD_ITEMS,
+			 	payload: res.data
+			})
+		 )
+}
+
+export const deleteItem = id  => {
 	return {
 		type: DELETE_ITEMS,
 		payload: id
 	}
+	// axios
+	// 	.delete(`/api/items/${id}`)
+	// 	.then(res => dispatch ({
+	// 		 	type : DELETE_ITEMS,
+	// 		 	payload: id
+	// 		})
+	// 	 )
 }
 
-export const addItem = (item) => {
+
+
+export const setItemsLoading = () => {
 	return {
-		type: ADD_ITEMS,
-		payload: item
+		type: ITEMS_LOADING
 	}
 }
